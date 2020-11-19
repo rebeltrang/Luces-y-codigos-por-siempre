68cb0e44f87b511f31a6530e97d21b4696777155
rebeltrang <74008512+rebeltrang@users.noreply.github.com>
2020-11-18 23:16:02 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,11 @@
+basic.forever(function () {
+    basic.showIcon(IconNames.Heart)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(500)
+})
